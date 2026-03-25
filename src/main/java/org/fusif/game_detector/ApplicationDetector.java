3bb8f9b57458721e95afabceefabb8e1a9555587
phayzeeVW <56519529+phayzeeVW@users.noreply.github.com>
2026-03-25 18:49:40 +0200

    Migrate to local serverless database - sqlite (#8)

--- src/main/java/org/fusif/game_detector/ApplicationDetector.java
+++ src/main/java/org/fusif/game_detector/ApplicationDetector.java
@@ -1,18 +1,36 @@
 package org.fusif.game_detector;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableScheduling
 @Slf4j
 public class ApplicationDetector {
 
+    @Value("${app.home")
+    private String home;
+
     public static void main(String[] args) {
         log.info("Starting application...");
         SpringApplication.run(ApplicationDetector.class, args);
     }
 
+    @EventListener(ApplicationStartingEvent.class)
+    public void ensureAppHomeExists() {
+        try {
+            Files.createDirectory(Path.of(home));
+        } catch (IOException e) {
+            log.error("Error creating app home directory", e);
+        }
+    }
 }
